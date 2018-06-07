bf4eb31b3218fe5df47b290910d0e4f4a5b4c639
Jeremy Pallats/starcraft.man <unknown3000@gmail.com>
2018-06-07 19:46:37 -0400

    Overdue cmake bump.

--- cmake.py
+++ cmake.py
@@ -10,11 +10,11 @@ class Cmake(Recipe):
         super(Cmake, self).__init__()
         self.homepage = 'www.cmake.org'
         self.repos = {
-            'stable': Archive('http://www.cmake.org/files/v3.3/'
-                              'cmake-3.3.2.tar.gz',
-                              hash='e75a178d6ebf182b048ebfe6e0657c49f0dc10'
-                              '9779170bad7ffcb17463f2fc22'),
-            'unstable': Git('git://cmake.org/cmake.git '),
+            'stable': Archive('http://www.cmake.org/files/v3.11/'
+                              'cmake-3.11.3.tar.gz',
+                              hash='287135b6beb7ffc1ccd02707271080bbf14c21'
+                              'd80c067ae2c0040e5f3508c39a'),
+            'unstable': Git('git://cmake.org/cmake.git'),
         }
         self.opts = {
             'bootstrap': '--sphinx-html --sphinx-man'
